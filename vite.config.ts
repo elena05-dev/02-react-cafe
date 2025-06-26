9c062279983f92721adf2ddb29e9f00067986335
Elena Polyakova <epolyk1970@gmail.com>
2025-06-26 22:47:52 +0300

    fix: correct base path to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/02-react-cafe/",
   build: {
+    outDir: "docs",
     sourcemap: true,
   },
+  plugins: [react()],
 });
