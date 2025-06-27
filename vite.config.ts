b09820b50868d04d796cb22ef044ff3f0b460956
Elena Polyakova <epolyk1970@gmail.com>
2025-06-27 15:50:01 +0300

    fix: update base and outDir for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/02-react-cafe/",
   build: {
     outDir: "dist",
     sourcemap: true,
