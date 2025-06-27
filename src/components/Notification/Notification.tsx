d3731bc9568110196643f7cb3f4ee2fa8268200d
Elena Polyakova <epolyk1970@gmail.com>
2025-06-27 19:02:58 +0300

    syntax correction

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,5 +1,5 @@
 import css from "./Notification.module.css";
 
-export default function NotifIcation() {
+export default function Notification() {
   return <p className={css.message}>No feedback yet</p>;
 }
--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,13 +1,17 @@
 import css from "./VoteOptions.module.css";
 import type { VoteType } from "../../types/votes";
 
-interface Props {
+interface VoteOptionsProps {
   onVote: (type: VoteType) => void;
   onReset: () => void;
   canReset: boolean;
 }
 
-export default function VoteOptions({ onVote, onReset, canReset }: Props) {
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VoteOptionsProps) {
   return (
     <div className={css.container}>
       <button className={css.button} onClick={() => onVote("good")}>
